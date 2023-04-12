d3459fbb6f9b9ba943a939ec2f2b3976d6c901ee
Alana Moore <polkafrogg@gmail.com>
2023-04-12 13:41:05 -0700

    Condense Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { ReactComponent as Mail } from '../img/mail-svg.svg';
 import { ReactComponent as Github } from '../img/github-svg.svg';
 import { ReactComponent as LinkedIn } from '../img/linkedin-svg.svg';
-import './Footer.css';
 
 const Wrapper = styled.div`
   left: 0;
@@ -21,20 +20,28 @@ const FooterText = styled.div`
   font-weight: 600;
 `
 
+const FooterLink = styled.a`
+  &:link,
+  &:visited {
+    color: #BD9A7A;
+    text-decoration: none;
+  }
+`
+
 const Footer = () => {
   return (
     <Wrapper>
       <FooterText>Let's connect!</FooterText>
-      <a href="mailto:polkafrogg@gmail.com" target="_blank" rel="noreferrer">
-          <Mail style={{ width: 40, height: 40, padding: 30, }} />
-      </a>
-      <a href="https://github.com/mooreala" target="_blank" rel="noreferrer">
-          <Github style={{ width: 50, height: 50, padding: 30, }} />
-      </a>
-      <a href="https://www.linkedin.com/in/alanaaemoore/" target="_blank" rel="noreferrer">
-          <LinkedIn style={{ width: 40, height: 40, padding: 30, }} />
-      </a>
-      <p>Website by me, powered by React! Check out the source code <a href="https://github.com/mooreala/react-portfolio" target="_blank" rel="noreferrer">here.</a></p>
+      <FooterLink href="mailto:polkafrogg@gmail.com" target="_blank" rel="noreferrer">
+          <Mail style={{ width: 40, height: 40, padding: 30, }} alt="Write Alana an email" />
+      </FooterLink>
+      <FooterLink href="https://github.com/mooreala" target="_blank" rel="noreferrer" >
+          <Github style={{ width: 50, height: 50, padding: 30, }} alt="Link to Alana's Github profile" />
+      </FooterLink>
+      <FooterLink href="https://www.linkedin.com/in/alanaaemoore/" target="_blank" rel="noreferrer">
+          <LinkedIn style={{ width: 40, height: 40, padding: 30, }} alt="Link to Alana's LinkedIn profile" />
+      </FooterLink>
+      <p>Website by me, powered by React! Check out the source code <FooterLink href="https://github.com/mooreala/react-portfolio" target="_blank" rel="noreferrer">here.</FooterLink></p>
     </Wrapper>
   );
 }
