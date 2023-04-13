fd93fadcfdcee66355c462bd6aeddc3e4b78a41c
Alana Moore <polkafrogg@gmail.com>
2023-04-12 22:54:26 -0700

    Edit hero size

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -19,7 +19,9 @@ function Intro() {
           I am passionate about many things such as music, art, and storytelling. My aspirations include working on creative software that can help others and impact their lives.
         </p>
       </div>
-      <img src={profile} width={300} height={300} alt='Self-portrait' />
+      <div className="portrait">
+        <img src={profile} width={300} height={300} alt='Self-portrait' /> 
+      </div>
     </div>
   );
 }
