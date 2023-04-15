680e4c405a4ba1177134710e8ea2f12926e30a7d
Alana Moore <polkafrogg@gmail.com>
2023-04-14 18:30:40 -0700

    Add project components to section

---
+++ src/projects/Project.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Github } from '../img/github-svg.svg';
+import { ReactComponent as ExtLink } from '../img/external-link-svg.svg';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 30px;
+  @media (max-width: 1087px) {
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin: auto;
+  }
+`
+
+const Card = styled.div`
+  margin-top: 0px;
+  flex-direction: column;
+  margin: 15px;
+  margin-top: 10px;
+`
+
+const ProjSection = styled.div`
+  max-width: 400px;
+  border-radius: 5px;
+  padding: 30px;
+  background-color: #4E3D42;
+  @media (max-width: 1087px) {
+    width: 70vw;
+    padding: 20px;
+    margin: 0px;
+  }
+`
+
+const LinkSection = styled.div`
+  flex-direction: row;
+  margin-top: 20px;
+  @media (max-width: 1087px) {
+    margin-top: 10px;
+  }
+`
+
+const TechStack = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  color: #B7B7A4;
+`
+
+const Title = styled.h1`
+  color: white;
+  font-size: 20px;
+`
+
+const Subtitle = styled.h2`
+  font-size: 15px;
+  color: #BD9A7A;
+`
+
+const Preview = styled.img`
+  /* max-width: 500px; */
+  border-radius: 5px;
+  width: 500px;
+  height: auto;
+  margin: 15px;
+  margin-top: 10px;
+  padding: 0px;
+  @media (max-width: 1087px) {
+    max-width: 500px;
+    width: 85%
+  }
+`
+
+const ProjLink = styled.a`
+  &:link,
+  &:visited {
+    color: #BD9A7A;
+    text-decoration: none;
+  }
+`
+
+function Project({src, title, subtitle, desc, stack, gitlink, extlink}) {
+  function renderGitLink() {
+    if (gitlink !== "") {
+      return (
+        <ProjLink href={gitlink} target="_blank" rel="noreferrer">
+          <Github style={{ width: 30, height: 30, margin: "0px 10px"}} />
+        </ProjLink>
+      );
+    }
+  }
+
+  function renderExtLink() {
+    if (extlink !== "") {
+      return (
+        <ProjLink href={extlink} target="_blank" rel="noreferrer">
+          <ExtLink style={{ width: 30, height: 30, margin: "0px 10px"}} />
+        </ProjLink>
+      );
+    }
+  }
+
+  return (
+    <Container>
+      <Preview src={src} />
+      <Card>
+        <ProjSection>
+          <Title>{title}</Title>
+          <Subtitle>{subtitle}</Subtitle>
+          <p style={{ color: "white" }}>{desc}</p>
+          <TechStack>
+            { stack.map(ln => (
+              <p style={{margin:10}}>{ln}</p>
+            ))}
+          </TechStack>
+        </ProjSection>
+        <LinkSection>
+          {renderGitLink()}
+          {renderExtLink()}
+        </LinkSection>
+      </Card>
+    </Container>
+  )
+};
+
+export default Project;
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,12 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container, Title, SectionLine } from '../SectionStyles';
+import Project from './Project';
+
+import dadproj from '../img/projects/dad.png';
+import calmcrows from '../img/projects/calmingcrows.png';
+import healthpot from '../img/projects/healthpotion.png';
+import kittykatch from '../img/projects/kittykatch.png';
+
+const ProjectSection = styled.div`
+  margin: 10px;
+`
+
+const projects = [
+  {
+    src: dadproj,
+    title: "ASU Senior Capstone",
+    subtitle: "Web Application",
+    desc: "A cloud-based responsive website that allows schools from any district to sign-up for an essay competition. Deliverables include source code in a git repository, DevOps CICD build and release Pipelines for the application, and design and architecture documentation.",
+    stack: [
+      "Node.js",
+      "React.js",
+      "Bootstrap",
+      "Azure",
+      "Github Actions",
+    ],
+    gitlink: "",
+    extlink: "https://dads-against-depression.github.io/school/",
+  },
+  {
+    src: calmcrows,
+    title: "Calming Crows",
+    subtitle: "Boids Behavior Simulation",
+    desc: "An interactive app that uses the flocking behaviors of boids to help calm users dealing with immediate anxiety or stress. This program will be available for users using a device with a version of Windows that supports raylib.",
+    stack: [
+      "C",
+      "Raylib",
+      "MinGW",
+    ],
+    gitlink: "https://github.com/mooreala/boids",
+    extlink: "https://www.youtube.com/watch?v=7dx2ekpEHAI",
+  },
+  {
+    src: healthpot,
+    title: "Health Potion",
+    subtitle: "Mobile Application Pitch Deck",
+    desc: "A proposal for a mental wellness app for gamers that provides users with a dashboard of features to provide wellness focused gaming.",
+    stack: [
+      "Figma",
+    ],
+    gitlink: "",
+    extlink: "https://docs.google.com/presentation/d/1MxQybgTJ5BlburDY5bnSdGqO5lM8eNTuOuHvD_js7Po/edit?usp=sharing",
+  },
+  {
+    src: kittykatch,
+    title: "Kitty Katch",
+    subtitle: "Unity Game",
+    desc: "A simple 2-D game made in Unity. All art assets made using Gimp. The goal of the game is to collect as many fish as possible, while dodging or destroying dropping fruits with your lazer gun.",
+    stack: [
+      "C#",
+      "Unity",
+      "Gimp",
+    ],
+    gitlink: "https://github.com/mooreala/kitty-katch",
+    extlink: "https://mooreala.itch.io/kitty-katch-demo",
+  },
+];
 
 function Projects() {
   return (
     <Container id="projects">
       <Title>PROJECTS</Title>
       <SectionLine />
+      <ProjectSection>
+        { projects.map(p => (
+          <Project src={p.src} title={p.title} subtitle={p.subtitle} desc={p.desc} stack={p.stack} gitlink={p.gitlink} extlink={p.extlink} />
+        ))}
+      </ProjectSection>
     </Container>
   );
 }
