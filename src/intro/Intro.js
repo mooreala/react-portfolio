f97e04bdf21b369b2e2a6fb53b79e234e3eaa3f5
Alana Moore <polkafrogg@gmail.com>
2024-08-01 15:22:25 -0700

    update my bio

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -63,11 +63,11 @@ function Intro() {
           I like to design, program, and draw.
         </IntroSubtitle>
         <p>
-          I am a recent Software Engineering graduate from ASU, and am interested in front-end development, human computer interaction, and AI. I just started a new role at Intel as a Software Application Development Engineer, where I was previously an intern! In my junior position I currently work on enterprise facing web-applications with regards to user experience design and back-end service development. 
+          I am a software developer slash design fanatic, with an interest in front-end development, human computer interaction, and AI. I am currently working as Software Application Development Engineer at Intel, and I work on enterprise facing web-applications with regards to user experience design and back-end service development!
         </p>
         <p>
           I am passionate about many things such as music, art, and storytelling. My aspirations include working on creative software that can help others and impact their lives.
-          In my free-time I'll either be reading fantasy novels, digital painting, or gaming! I am currently on my second Breath of the Wild playthrough.
+          In my free-time I'll either be reading fantasy novels, digital painting, or gaming! I am currently trying to finish BG3 Act III.
         </p>
       </IntroText>
       <Portrait src={profile} alt='Self-portrait' /> 
